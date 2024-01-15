6a353688371e748a4dd84f233879b547c88563bf
Manjot Virdi <virdi_manjot@hotmail.com>
2024-01-14 17:01:30 -0800

    set up apollo server and inital schemas

---
+++ index.js
@@ -0,0 +1,45 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// db
+import db from "./_db.js";
+
+//types
+import { typeDefs } from "./schema.js";
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games;
+        },
+        game(_, args) {
+            return db.games.find((game) => game.id === args.id);
+        },
+        reviews() {
+            return db.reviews;
+        },
+        review(_, args) {
+            return db.reviews.find((review) => review.id === args.id);
+        },
+        authors() {
+            return db.authors;
+        },
+        author(_, args) {
+            return db.authors.find((author) => author.id === args.id);
+        },
+    },
+};
+
+//server setup
+const server = new ApolloServer({
+    //typeDefs -- definitions of types of data
+    typeDefs,
+    //resolvers
+    resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+
+console.log("server ready at port", 4000);
